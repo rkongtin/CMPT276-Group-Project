b795fc16750fe377ca053c4ce045c38613c622e7
tplamondon <tplamond@sfu.ca>
2016-02-19 18:38:31 +0000

    fixed bug preventing db:migrate from being run (automation did syntax error in migration file

--- db/migrate/20160217190122_create_users.rb
+++ db/migrate/20160217190122_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.name :string
-      t.email :string
-      t.password :string
+      t.string :name
+      t.string :email
+      t.string :password
 
       t.timestamps null: false
     end
