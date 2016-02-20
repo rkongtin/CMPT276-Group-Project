1b04ec92b3b9d09548ee2a9a410c700c807d0880
tplamondon <tplamond@sfu.ca>
2016-02-20 21:39:18 +0000

    fixed migration issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
     before_save { self.email = email.downcase }
+    
     validates :name, :email, :password, presence: true
     validates :email, uniqueness: { case_sensitive: false } #Tutorial said doing this, rails will infer that uniqueness should be true
     has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
+    validates :password, presence: true, length: { minimum: 6 }, confirmation: true
 end
