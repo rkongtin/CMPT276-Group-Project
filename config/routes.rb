8931c6bc9058b35a5c5ff3df066faa06d5a46137
Charles Tsui <charles.tsui@shaw.ca>
2016-02-22 05:42:21 +0000

    made it pretty, but still lots to do

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'static_pages/about' => 'static_pages#about'
   get 'static_pages/help' => 'static_pages#help'
 
-  root 'sessions#new'
+  root 'static_pages#home'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
