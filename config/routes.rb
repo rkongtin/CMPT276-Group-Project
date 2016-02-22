3772bd01f336637cb930ca56bde76c2875b4eb63
Charles Tsui <charles.tsui@shaw.ca>
2016-02-22 02:28:24 +0000

    did some stuff with links and stuff. will continue working on it after dinner :P

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home' => 'static_pages#home'
+  get 'static_pages/about' => 'static_pages#about'
+  get 'static_pages/help' => 'static_pages#help'
+
   root 'sessions#new'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
