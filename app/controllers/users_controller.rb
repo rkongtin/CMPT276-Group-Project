65b02402c8e2632919e45bdb6362828a3332a6ac
tplamondon <tplamond@sfu.ca>
2016-03-11 06:13:48 +0000

    weird behaviour listing all users, edit still not working, everyone has access to users/index for some reason

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  
+  before_action :requireIsAdmin, only: [:index]
     
   def index
-    @user = User.all  
+    @users = User.all  
   end
   
 
@@ -14,6 +16,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def edit
+    @user = User.find(params[:id])  
+  end
+  
   #don't have their own view
   def create
     @user = User.new(user_params)
@@ -24,10 +30,30 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def update
+    @user = user.find(params[:id])
+  
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def requireIsAdmin #code to require to be admin, others get redirected back
+      begin
+        unless current_user.admin?
+          redirect_to users_home_path
+        end
+      rescue nil  #this will run if user isn't logged in, so current_user.admin? fails, just redirect them to login
+        redirect_to sessions_new_path
+      end
     end
     
 end
