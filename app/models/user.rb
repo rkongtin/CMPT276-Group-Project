94be8ed82614d86a24033f868b3d17b374f9b250
Yong Lee <supykl@hotmail.com>
2016-03-15 02:37:55 +0000

    map display problem on c9 solved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
     #before_save { self.email = email.downcase }
-    
+
     #validates :name, :email, :password, presence: true
     #validates :email, uniqueness: { case_sensitive: false } #Tutorial said doing this, rails will infer that uniqueness should be true
     #
     #validates :password, presence: true, length: { minimum: 6 }, confirmation: true
-    
-    before_save { self.email = email.downcase }
+
+    before_save { self.email = email.to_s.downcase.gsub(/\s+/, '') } #bug is fixed, this converts everything to string; but afterwards updated email shows blank - Yong
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
         uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
-    
+
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                       BCrypt::Engine.cost
