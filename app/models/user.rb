c9bd649df7374c95dcb2b6e47d7497e5a786f5ca
tplamondon <tplamond@sfu.ca>
2016-03-26 23:48:41 +0000

    removed some junk in users model, and deleted some useless pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,4 @@
 class User < ActiveRecord::Base
-    #before_save { self.email = email.downcase }
-
-    #validates :name, :email, :password, presence: true
-    #validates :email, uniqueness: { case_sensitive: false } #Tutorial said doing this, rails will infer that uniqueness should be true
-    #
-    #validates :password, presence: true, length: { minimum: 6 }, confirmation: true
 
     before_save { self.email = email.to_s.downcase.gsub(/\s+/, '') } #(datatype) bug is fixed, this converts everything to string; but afterwards updated email shows blank - Yong
     validates :name, presence: true, length: { maximum: 50 }
