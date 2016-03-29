d7e5486b7cdd0dfdf7c4554a46c9db5c8b0f7a8f
tplamondon <tplamond@sfu.ca>
2016-03-29 20:37:48 +0000

    some testing of house model done

--- test/models/house_test.rb
+++ test/models/house_test.rb
@@ -4,4 +4,30 @@ class HouseTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "valid" do #test a random thing that SHOULD be valid
+    house = House.new(:address => "1111 test av", :price => 60000, :sqft => 500, :amenities => "Stuff, please can we use square metres?", :contact_info => "911", :pictures => "?")
+    assert house.save
+  end
+  
+  test "addressNotBlank" do
+    house = House.new(:address => "", :price => 60000, :sqft => 500, :amenities => "Stuff, please can we use square metres?", :contact_info => "911")
+    assert_not house.save
+  end
+  
+  test "priceNotNull" do
+    house = House.new(:address => "1111 test av", :sqft => 500, :amenities => "Stuff, please can we use square metres?", :contact_info => "911")
+    assert_not house.save
+  end
+  
+  test "sqftNotNull" do
+    house = House.new(:address => "1111 test av", :price => 60000, :amenities => "Stuff, please can we use square metres?", :contact_info => "911")
+    assert_not house.save
+  end
+  
+  test "contactInfoNotNull" do
+    house = House.new(:address => "1111 test av", :price => 60000, :sqft => 500, :amenities => "Stuff, please can we use square metres?")
+    assert_not house.save
+  end
+  
 end
