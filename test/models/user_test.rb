4ffffe1a74e0631b03b40baab7ee84d066713abc
tplamondon <tplamond@sfu.ca>
2016-03-29 19:45:23 +0000

    removed all tests that were created automatically (some were weird and buggy, as in one in test/integration)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,10 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "name_not_nil" do
+    user = User.new
+    assert_not user.save
+  end
+  
+  
 end
