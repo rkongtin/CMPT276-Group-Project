fc248539b38bc66bc7a34333dd856b07f6feda23
tplamondon <tplamond@sfu.ca>
2016-03-31 23:31:27 +0000

    fixed broken link in house controller, edited house_test, program/tests complaining about imageMagick, don't know how to fix

--- test/models/house_test.rb
+++ test/models/house_test.rb
@@ -5,33 +5,37 @@ class HouseTest < ActiveSupport::TestCase
   #   assert true
   # end
   
+  def setup
+    @pic = 'http://s1.narvii.com/image/mud4owqv2tonms3hkdpbyxbpy4imm3zh_hq.jpg'
+  end
+  
   test "valid" do #test a random thing that SHOULD be valid
-    house = House.new(:address => "1111 test av", :price => 60000, :sqft => 500, :amenities => "Stuff, please can we use square metres?", :contact_info => "911", :pictures => "?")
+    house = House.new(:address => "1111 test av", :price => 60000, :sqft => 500, :amenities => "Stuff, please can we use square metres?", :contact_info => "911", :pictures => @pic)
     assert house.save
   end
   
   test "addressNotBlank" do
-    house = House.new(:address => "", :price => 60000, :sqft => 500, :amenities => "Stuff, please can we use square metres?", :contact_info => "911", :pictures => "?")
+    house = House.new(:address => "", :price => 60000, :sqft => 500, :amenities => "Stuff, please can we use square metres?", :contact_info => "911", :pictures => @pic)
     assert_not house.save
   end
   
   test "priceNotNull" do
-    house = House.new(:address => "1111 test av", :sqft => 500, :amenities => "Stuff, please can we use square metres?", :contact_info => "911", :pictures => "?")
+    house = House.new(:address => "1111 test av", :sqft => 500, :amenities => "Stuff, please can we use square metres?", :contact_info => "911", :pictures => @pic)
     assert_not house.save
   end
   
   test "sqftNotNull" do
-    house = House.new(:address => "1111 test av", :price => 60000, :amenities => "Stuff, please can we use square metres?", :contact_info => "911", :pictures => "?")
+    house = House.new(:address => "1111 test av", :price => 60000, :amenities => "Stuff, please can we use square metres?", :contact_info => "911", :pictures => @pic)
     assert_not house.save
   end
   
   test "contactInfoNotNull" do
-    house = House.new(:address => "1111 test av", :price => 60000, :sqft => 500, :amenities => "Stuff, please can we use square metres?", :pictures => "?")
+    house = House.new(:address => "1111 test av", :price => 60000, :sqft => 500, :amenities => "Stuff, please can we use square metres?", :pictures => @pic)
     assert_not house.save
   end
   
   test "ammenitiesNotNull" do
-    house = House.new(:address => "1111 test av", :price => 60000, :sqft => 500, :contact_info => "911", :pictures => "?")
+    house = House.new(:address => "1111 test av", :price => 60000, :sqft => 500, :contact_info => "911", :pictures => @pic)
     assert_not house.save
   end
   
@@ -41,12 +45,12 @@ class HouseTest < ActiveSupport::TestCase
   end
   
   test "priceIsNum" do
-    house = House.new(:address => "1111 test av", :price => "0XAF5", :sqft => 500, :amenities => "Stuff, please can we use square metres?", :contact_info => "911", :pictures => "?")
+    house = House.new(:address => "1111 test av", :price => "0XAF5", :sqft => 500, :amenities => "Stuff, please can we use square metres?", :contact_info => "911", :pictures => @pic)
     assert_not house.save
   end
   
   test "sqftIsNum" do
-    house = House.new(:address => "1111 test av", :price => 60000, :sqft => "50a12", :amenities => "Stuff, please can we use square metres?", :contact_info => "911", :pictures => "?")
+    house = House.new(:address => "1111 test av", :price => 60000, :sqft => "50a12", :amenities => "Stuff, please can we use square metres?", :contact_info => "911", :pictures => @pic)
     assert_not house.save
   end
   
