ac42dc9d8a962e8121bdf132875722a9d19dbd1f
tplamondon <tplamond@sfu.ca>
2016-04-03 23:00:16 +0000

    edited db:seed seed valid (uses picture now) users again

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,6 @@
 #rake db:reset to reset the db
 #rake db:seed to set up db with blow things
 
-User.create(name: 'FirstAdmin', email: 'admin@sfu.ca', password: 'administration', admin: 'true')
-User.create(name: 'test', email: 'test@sfu.ca', password: 'tester', admin: 'false')
-User.create(name: 'test2', email: 'test2@sfu.ca', password: 'tester', admin: 'false')
+User.create(name: 'FirstAdmin', email: 'admin@sfu.ca', password: 'administration', admin: 'true', pictures: "http://cdn.myanimelist.net/images/characters/3/243573.jpg")
+User.create(name: 'test', email: 'test@sfu.ca', password: 'tester', admin: 'false', pictures: "http://orig00.deviantart.net/f31f/f/2015/068/4/5/profile_picture_by_kotatux-d8l11ni.png")
+User.create(name: 'test2', email: 'test2@sfu.ca', password: 'tester', admin: 'false', pictures: "http://assets2.ignimgs.com/2015/12/12/toarukagakunorailguns-19-febri-meatball-eating-cute-comedy-adorablejpg-19ba09_1280w.jpg")
